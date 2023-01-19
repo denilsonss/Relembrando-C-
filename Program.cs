00b4c8df03a24eb6704633f46005dbefd1ad2abd
Denilson De Sousa Soares <jhondoe@exemple.com>
2023-01-19 17:31:20 -0300

    Primeira ATV

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿
+// Descobrindo idade.
+
+Console.WriteLine("Digite seu nome: ");
+string nome = Console.ReadLine();
+Console.WriteLine("Qual ano você nasceu?");
+int nascimento =  int.Parse(Console.ReadLine());
+int year = 2023;
+int age = year - nascimento;
+Console.WriteLine(nome + ", sua idade é: " + age + " anos.");
+
+if(age <= 18)
+    {
+    Console.WriteLine("Você é menor de idade");
+    }    
+ else 
+    {
+    Console.WriteLine("Você é maior de idade");
+    }
+
+
+/*  comentários:
+
+tipos mais comuns em c#:
+
+bool
+char
+byte
+int
+long 
+float
+doble
+decimal
+string
+
+*/
+
+
+
+
+
